66b5715cf8a8b945195d6be49e84025d415e0c43
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-03-17 17:52:55 -0400

    get category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,7 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product}]
+      include: [{ model: Product }]
     });
     res.status(200).json(categoryData)
   } catch (error) {
@@ -14,9 +14,17 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    })
+    res.status(200).json(categoryData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.post('/', (req, res) => {
