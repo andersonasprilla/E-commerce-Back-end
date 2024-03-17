689a4093d60f19218a19863dd7717afe58fafcb8
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-03-17 18:32:55 -0400

    post request category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,8 +27,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name
+    })
+    res.status(200).json(categoryData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.put('/:id', (req, res) => {
